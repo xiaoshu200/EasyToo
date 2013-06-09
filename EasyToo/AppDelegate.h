c54a1273582d88d602fd916109eaeedc414bbac8
郑 哲 <ottozheng@ottozheng-nb.local>
2013-06-09 17:46:08 +0800

    Initial Commit

---
+++ EasyToo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EasyToo
+//
+//  Created by 郑 哲 on 6/9/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EasyToo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EasyToo
+//
+//  Created by 郑 哲 on 6/9/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
